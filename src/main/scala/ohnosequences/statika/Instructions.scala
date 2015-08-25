a806a4d3e04fb947d572b721fd6eed81bf998503
Alexey Alekhin <laughedelic@gmail.com>
2015-08-25 14:42:13 +0200

    Revert "Significant changes in the instructions API; WIP"
    This reverts commit a29a72ed547499d5725c0b58d19e3609dd979049.

--- src/main/scala/ohnosequences/statika/Instructions.scala
+++ src/main/scala/ohnosequences/statika/Instructions.scala
@@ -21,23 +21,22 @@ case object instructions {
     def prependTrace(msgs: Seq[String]): Result[Out]
   }
 
-  abstract class Result[O] extends AnyResult { type Out = O }
 
-  case class Failure[O](val trace: Seq[String]) extends Result[O] {
-    // type Out = O
+  case class Failure[O](val trace: Seq[String]) extends AnyResult {
+    type Out = O
     val hasFailures = true
 
     def prependTrace(msgs: Seq[String]): Failure[Out] = Failure[O](msgs ++ trace)
   }
-  case class Success[O](val trace: Seq[String], o: O) extends Result[O] {
-    // type Out = O
+  case class Success[O](val trace: Seq[String], o: O) extends AnyResult {
+    type Out = O
     val  out: Out = o
     val hasFailures = false
 
     def prependTrace(msgs: Seq[String]): Success[O] = Success[O](msgs ++ trace, o)
   }
 
-  // type Result[O] = AnyResult { type Out <: O }
+  type Result[O] = AnyResult { type Out <: O }
 
 
   implicit def resultSyntax[O](r: Result[O]): ResultSyntax[O] = ResultSyntax[O](r)
@@ -46,200 +45,136 @@ case object instructions {
   }
 
 
-  /* Environment abstracts over the execution context */
-  trait AnyEnvironment
 
-  /* But at the moment we are going to use simple file system environment: */
-  trait AnyFileSystemEnvironment extends AnyEnvironment {
-
-    val workingDir: File
-  }
-
-  case class FileSystemEnvironment(val workingDir: File) extends AnyFileSystemEnvironment
-
-  /* Would be nice to be able to combine different environments and express it on type level,
-     but at the moment there is no real need in that and no easy way to do it. */
-
-  /* This is kind of an arrow description of type `In => Result[Out]` */
   trait AnyInstructions {
-    type In
     type Out
+    type R = Result[Out]
 
-    /* instructions know about minimal neccessary environment for its execution */
-    type Env <: AnyEnvironment
-
-    def run(env: Env, in: In): Result[Out]
+    def run(workingDir: File): R
   }
 
-  object AnyInstructions {
-
-    type sameAs[I <: AnyInstructions] = I with AnyInstructions {
-      type In  = I#In
-      type Out = I#Out
-      type Env = I#Env
-    }
-  }
-
-  /* We are going to combine instructions in the following way
-
-     ```
-     A => Result[B]
-     C => Result[D] where C <: B
-     --------------
-     A => Result[D]
-     ```
-
-     i.e it's a `flatMap`, but with different flavours, depending on how
-     we want to process result (failure or success) of the first action.
-  */
   trait AnyCombinedInstructions extends AnyInstructions {
     type First <: AnyInstructions
-    val  first: AnyInstructions.sameAs[First]
+    val  first: First
 
-    type Second <: AnyInstructions {
-      type In  = First#Out
-      // NOTE: this is probably too strict, but fine for now
-      type Env = First#Env
-    }
-    val  second: AnyInstructions.sameAs[Second]
+    type Second <: AnyInstructions
+    val  second: Second
 
-    type Env = First#Env
-    type In  = First#In
     type Out = Second#Out
   }
 
+
   /* Same as combine non-failable, but doesn't append the first trace ("forgets it") */
-  case class -&-[
+  case class CombineNonFailable[
     F <: AnyInstructions,
-    S <: AnyInstructions { type In = F#Out; type Env = F#Env }
-  ](val first:  AnyInstructions.sameAs[F],
-    val second: AnyInstructions.sameAs[S]
-  ) extends AnyCombinedInstructions  {
+    S <: AnyInstructions
+  ](val first: F,
+    val second: S
+  ) extends AnyCombinedInstructions {
     type First = F
     type Second = S
 
-    def run(env: Env, in: In): Result[Out] = {
-      first.run(env, in) match {
-        case Failure(tr) => Failure(tr)
-        case Success(tr, out) => tr +: second.run(env, out)
+    final def run(workingDir: File): Result[Out] = {
+      first.run(workingDir) match {
+        case Failure(tr)  => Failure(tr)
+        case Success(tr1, x) => tr1 +: second.run(workingDir)
       }
     }
   }
 
-  // type -&-[
-  //   F <: AnyInstructions,
-  //   S <: AnyInstructions { type In = F#Out; type Env = F#Env }
-  // ] = CombineNonFailable[F, S]
+  type -&-[F <: AnyInstructions, S <: AnyInstructions] = CombineNonFailable[F, S]
 
 
-  // /* Same as combine non-failable, but doesn't append the first trace ("forgets it") */
-  // case class CombineForgetful[
-  //   F <: AnyInstructions,
-  //   S <: AnyInstructions { type In = F#Out; type Env = F#Env }
-  // ](val first:  AnyInstructions.sameAs[F],
-  //   val second: AnyInstructions.sameAs[S]
-  // ) extends AnyCombinedInstructions {
-  //   type First = F
-  //   type Second = S
-  //
-  //   def run(env: Env, in: In): Result[Out] = {
-  //     first.run(env, in) match {
-  //       case Failure(tr)     => Failure(tr)
-  //       case Success(tr1, _) => second.run(workingDir)
-  //     }
-  //   }
-  // }
-  //
-  // type ->-[F <: AnyInstructions, S <: AnyInstructions] = CombineForgetful[F, S]
+  /* Same as combine non-failable, but doesn't append the first trace ("forgets it") */
+  case class CombineForgetful[
+    F <: AnyInstructions,
+    S <: AnyInstructions
+  ](val first: F,
+    val second: S
+  ) extends AnyCombinedInstructions {
+    type First = F
+    type Second = S
 
-  // case class CombineFailable[
-  //   // FIXME: sbt warns that this is bad refinement:
-  //   F <: AnyInstructions,
-  //   S <: AnyInstructions { type In = F#Out with F#In; type Env = F#Env }
-  // ](val first:  AnyInstructions.sameAs[F],
-  //   val second: AnyInstructions.sameAs[S]
-  // ) extends AnyCombinedInstructions {
-  //   type First = F
-  //   type Second = S
-  //
-  //   def run(env: Env, in: In): Result[Out] = {
-  //     first.run(env, in) match {
-  //       case Failure(tr)      => tr +: second.run(env, in)
-  //       case Success(tr, out) => tr +: second.run(env, out)
-  //     }
-  //
-  //   }
-  // }
+    final def run(workingDir: File): Result[Out] = {
+      first.run(workingDir) match {
+        case Failure(tr)  => Failure(tr)
+        case Success(_, tr1) => second.run(workingDir)
+      }
+    }
+  }
 
-  // type -|-[
-  //   F <: AnyInstructions { type Out = F#In },
-  //   S <: AnyInstructions { type In = F#Out; type Env = F#Env }
-  // ] = CombineFailable[F, S]
+  type ->-[F <: AnyInstructions, S <: AnyInstructions] = CombineForgetful[F, S]
+
+
+  case class CombineFailable[
+    F <: AnyInstructions,
+    S <: AnyInstructions
+  ](val first: F,
+    val second: S
+  ) extends AnyCombinedInstructions {
+    type First = F
+    type Second = S
+
+    final def run(workingDir: File): Result[Out] = {
+      first.run(workingDir).trace +: second.run(workingDir)
+    }
+  }
+
+  type -|-[F <: AnyInstructions, S <: AnyInstructions] = CombineFailable[F, S]
+
+
+  implicit def instructionsSyntax[X, I <: AnyInstructions](x: X)(implicit toInst: X => I):
+    InstructionsSyntax[I] =
+    InstructionsSyntax[I](toInst(x))
+
+  case class InstructionsSyntax[I <: AnyInstructions](i: I) {
+
+    def -&-[U <: AnyInstructions](u: U): I -&- U = CombineNonFailable[I, U](i, u)
+    def ->-[U <: AnyInstructions](u: U): I ->- U = CombineForgetful[I, U](i, u)
+    def -|-[U <: AnyInstructions](u: U): I -|- U = CombineFailable[I, U](i, u)
+  }
 
 
   import sys.process.Process
   import util.Try
 
-  trait AnySimpleInstructions extends AnyInstructions {
+  trait AnySimpleInstructions extends AnyInstructions
 
-    type Env = AnyFileSystemEnvironment
-  }
-
-  abstract class SimpleInstructions[I, O] extends AnySimpleInstructions {
-
-    type In = I
+  class SimpleInstructions[O](r: File => Result[O]) extends AnySimpleInstructions {
     type Out = O
+
+    def run(workingDir: File): Result[Out] = r(workingDir)
   }
 
 
-  case class say[I](msg: String) extends SimpleInstructions[I, I] {
+  case class say(msg: String) extends SimpleInstructions[Unit](
+    _ => Success[Unit](Seq(msg), ())
+  )
 
-    def run(env: Env, in: In): Result[Out] = Success[I](Seq(msg), in)
-  }
-
-  case class fail[I](msg: String) extends SimpleInstructions[I, I] {
-
-    def run(env: Env, in: In): Result[Out] = Failure[I](Seq(msg))
-  }
+  case class fail(msg: String) extends SimpleInstructions[Unit](
+    _ => Failure[Unit](Seq(msg))
+  )
 
 
-  case class TryInstructions[I, O](tf: I => Try[O]) extends SimpleInstructions[I, O] {
-
-    def run(env: Env, in: In): Result[Out] = {
-      tf(in) match {
-        case util.Success(output) => Success[O](Seq(tf.toString), output)
-        case util.Failure(e) => Failure[O](Seq(e.getMessage))
-      }
+  case class TryInstructions[X](t: Try[X]) extends SimpleInstructions[X]({
+    _ => t match {
+      case util.Success(output) => Success[X](Seq(t.toString), output)
+      case util.Failure(e) => Failure[X](Seq(e.getMessage))
     }
-  }
+  })
 
-  implicit def tryToInstructions[I, O](t: Try[O]): TryInstructions[I, O] = TryInstructions[I, O](_ => t)
+  implicit def tryToInstructions[T](t: Try[T]): TryInstructions[T] = TryInstructions[T](t)
 
 
-  case class CmdInstructions[I](sf: I => Seq[String]) extends SimpleInstructions[I, String] {
-
-    def run(env: Env, in: In): Result[Out] = {
-      val seq = sf(in)
-      Try( Process(seq, env.workingDir).!! ) match {
+  case class CmdInstructions(seq: Seq[String]) extends SimpleInstructions[String]({
+    workingDir: File =>
+      Try( Process(seq, workingDir).!! ) match {
         case util.Success(output) => Success[String](Seq(seq.mkString(" ")), output)
         case util.Failure(e) => Failure[String](Seq(e.getMessage))
       }
-    }
-  }
+  })
 
-  def cmd[I](command: String)(args: String*): CmdInstructions[I] = CmdInstructions[I](_ => command +: args)
-
-  implicit def seqToInstructions[I](s: Seq[String]): CmdInstructions[I] = CmdInstructions[I](_ => s)
-
-
-  implicit def instructionsSyntax[X, I <: AnyInstructions](x: X)(implicit toInst: X => AnyInstructions.sameAs[I]):
-    InstructionsSyntax[I] =
-    InstructionsSyntax[I](toInst(x))
-
-  case class InstructionsSyntax[I <: AnyInstructions](i: AnyInstructions.sameAs[I]) {
-
-    def -&-[U <: AnyInstructions { type In = I#Out; type Env = I#Env }]
-      (u: AnyInstructions.sameAs[U]): I -&- U = instructions.-&-[I, U](i, u)
-  }
+  def cmd(command: String)(args: String*): CmdInstructions = CmdInstructions(command +: args)
+  implicit def seqToInstructions(s: Seq[String]): CmdInstructions = CmdInstructions(s)
 
 }
