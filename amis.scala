ba5dc9bfaffa27b295ac15424f164f84ceea70aa
Alexey Alekhin <laughedelic@gmail.com>
2015-12-02 19:59:00 +0100

    Updated to the aws-scala-tools 0.15.0

--- amis.scala
+++ amis.scala
@@ -156,16 +156,13 @@ case class LinuxAMICompSyntax[C <: AnyLinuxAMICompatible](val comp: C) {
     instanceMonitoring: Boolean = false,
     deviceMapping: Map[String, String] = Map[String, String]()
   )(implicit
-    supportsStorage: IT SupportsStorageType AMI#Storage,
-    supportsVirt:    IT SupportsVirtualization AMI#Virt
+    supportsAMI: IT SupportsAMI AMI
   ): LaunchSpecs[InstanceSpecs[AMI, IT]] =
     LaunchSpecs(
       InstanceSpecs[AMI, IT](
         comp.environment.ami,
         instanceType
-      )(supportsStorage,
-        supportsVirt
-      )
+      )(supportsAMI)
     )(keyPair,
       userScript,
       instanceProfile,
